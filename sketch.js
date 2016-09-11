c5d58ad79063e83da08af33801d4444d30c0d550
Julian Jaramillo <jjaram48@gmail.com>
2016-09-11 14:10:05 +0200

    implemented color/freq array

--- sketch.js
+++ sketch.js
@@ -1,16 +1,17 @@
 var x;
-var col = 0;
+var col = [];
 
 function setup() {
   createCanvas(400,400);
 }
 
+
+
 function draw() {
 background(0);
   for (var x = 0; x < width; x++) {
-    col = map(x, 0, width, 0, 255);
-    stroke(col);
+    col [x]= map(x, 0, width, 0, 255);
+    stroke(col[x]);
     line( x, 0, x, height);
   }
-
 }
