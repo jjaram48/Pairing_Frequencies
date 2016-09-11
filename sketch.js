f2a91c54e6e45d22e153d7844d11afd06ffc842e
Julian Jaramillo <jjaram48@gmail.com>
2016-09-11 14:17:32 +0200

    organized freq_generate function

--- sketch.js
+++ sketch.js
@@ -1,17 +1,20 @@
-var x;
 var col = [];
 
 function setup() {
   createCanvas(400,400);
 }
 
-
+function freq_generate (){
+  for (var i = 0; i < width; i++) {
+    col [i]= map(i, 0, width, 0, 255);
+  }
+}
 
 function draw() {
 background(0);
-  for (var x = 0; x < width; x++) {
-    col [x]= map(x, 0, width, 0, 255);
-    stroke(col[x]);
-    line( x, 0, x, height);
+  freq_generate();
+  for (var i = 0; i < col.length; i++) {
+    stroke(col[i]);
+    line( i, 0, i, height)
   }
 }
