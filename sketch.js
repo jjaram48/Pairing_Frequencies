18382020a1b6c6f0f22937f5fbf4d7684184c906
Julian Jaramillo <jjaram48@gmail.com>
2016-09-11 18:15:15 +0200

    Mover generate freq to setup and printed total average

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 var col = [];       //  STORES ALL SINGLE VALUES
 var avg_col= [];    //  STORES THE FINAL AVERAGES
-var avgs = 3;       //  DEFINES THE NUMBER OF AVERAGES
+var num_avgs = 3;       //  DEFINES THE NUMBER OF AVERAGES
+var part_avgs = []; // STORES PARTIAL AVERAGES
 
 function setup() {
   createCanvas(400,400);
+  freq_generate();
 }
 
 function freq_generate (){    // GENERATES THE SPECTRUM OF FREQUENCIES
@@ -12,9 +14,28 @@ function freq_generate (){    // GENERATES THE SPECTRUM OF FREQUENCIES
   }
 }
 
-function calc_total_average (){
+function calc_total_average (){     //  TOTAL AVERAGE
   var sum = 0;
   for( var i = 0; i < col.length; i++ ){
+      sum += parseInt( col[i], 10 ); //don't forget to add the base 10
+  }
+  var avgs = sum/col.length;
+
+  for( var i = 0; i < col.length; i++ ){
+       col[i] = avgs;
+  }
+    println(num_avgs);
+}
+
+function calc_partial_averages (){    //  PARTIAL AVERAGES
+
+for (var i = 0; i < col.length/num_avgs; i++) { //
+  var sum = 0;
+  part_avgs[i]
+}
+
+
+  for( var j = 0; j < col.length; j++ ){
       sum += parseInt( col[i], 10 ); //don't forget to add the base 10
   }
   var avgs = sum/col.length;
@@ -23,10 +44,10 @@ function calc_total_average (){
 
 function draw() {
 background(0);
-  freq_generate();
+  calc_total_average();
   for (var i = 0; i < col.length; i++) {    //  PLOTS THE FREQUENCIES ACCORDING
     stroke(col[i]);
     line( i, 0, i, height)
   }
-  calc_total_average();
+  // calc_partial_averages();
 }
